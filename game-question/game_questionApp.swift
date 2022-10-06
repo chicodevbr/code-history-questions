86ca683061fc04c9663274515c634d1e24df0c5c
chicodevbr <chicodevbr@gmail.com>
2022-10-06 19:55:00 -0300

    Initial Commit

---
+++ game-question/game_questionApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_questionApp.swift
+//  game-question
+//
+//  Created by fjunior on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct game_questionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
