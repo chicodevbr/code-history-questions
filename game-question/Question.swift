755cbdacda3a0721d6b4f5991b13ed64dd498602
chicodevbr <chicodevbr@gmail.com>
2022-10-09 12:21:29 -0300

    initial project

---
+++ game-question/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  game-question
+//
+//  Created by fjunior on 06/10/22.
+//
+
+import Foundation
+
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
