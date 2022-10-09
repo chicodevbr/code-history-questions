4a99b069c8d21724523d3596ae6124c75fa490c4
chicodevbr <chicodevbr@gmail.com>
2022-10-09 18:35:05 -0300

    adding game model

---
+++ game-question/Models/Question.swift
@@ -0,0 +1,36 @@
+//
+//  Question.swift
+//  game-question
+//
+//  Created by fjunior on 06/10/22.
+//
+
+import Foundation
+
+
+struct Question: Hashable {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                possibleAnswers: [
+                "Steve Jobs",
+                "Linus Torvalds",
+                "Bill Gates",
+                "Tim Berners-Lee"],
+                correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                possibleAnswers: [
+                "Simula",
+                "Python",
+                "Swift",
+                "C"
+                
+                ],
+                correctAnswerIndex: 0)
+    ]
+    
+    
+}
